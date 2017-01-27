76dca446bd9d7251c9985a24cdc82dfb62938e01
George Hudson <georgehudson78@gmail.com>
2017-01-27 16:05:55 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  # TEST this error
+  # TEST this working
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
