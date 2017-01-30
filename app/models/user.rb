bd404653620b529dfbc02181e19d1a53e82e619f
George Hudson <georgehudson78@gmail.com>
2017-01-30 15:58:24 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  # Update on PR?
+  # Update on error?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
